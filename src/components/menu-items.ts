57d555405855ba33027c378fd81407e2faa35b4e
Andy <onsubmit@outlook.com>
2025-03-09 12:53:08 -0700

    Align key combos

---
+++ src/components/menu-items.ts
@@ -0,0 +1,87 @@
+export type MenuItem = {
+  title: string;
+  subItems?: Array<MenuItem>;
+  keyCombo?: string;
+};
+
+export const menuItems: Array<MenuItem> = [
+  {
+    title: 'File',
+    subItems: [
+      {
+        title: 'New',
+      },
+      {
+        title: 'Open...',
+      },
+      {
+        title: 'Save',
+      },
+      {
+        title: 'Save As...',
+      },
+      {
+        title: 'Close',
+      },
+    ],
+  },
+  {
+    title: 'Edit',
+    subItems: [
+      {
+        title: 'Cut',
+        keyCombo: 'Ctrl+X',
+      },
+      {
+        title: 'Copy',
+        keyCombo: 'Ctrl+C',
+      },
+      {
+        title: 'Paste',
+        keyCombo: 'Ctrl+V',
+      },
+      {
+        title: 'Clear',
+        keyCombo: 'Del',
+      },
+    ],
+  },
+  {
+    title: 'Search',
+    subItems: [
+      {
+        title: 'Find...',
+      },
+      {
+        title: 'Repeat Last Find',
+        keyCombo: 'F3',
+      },
+      {
+        title: 'Replace',
+      },
+    ],
+  },
+  {
+    title: 'View',
+    subItems: [
+      {
+        title: 'Split Window',
+        keyCombo: 'Ctrl+F6',
+      },
+      {
+        title: 'Size Window',
+        keyCombo: 'Ctrl+F8',
+      },
+      {
+        title: 'Close Window',
+        keyCombo: 'Ctrl+F4',
+      },
+    ],
+  },
+  {
+    title: 'Options',
+  },
+  {
+    title: 'Help',
+  },
+];
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,71 +1,22 @@
 import { useState } from 'react';
 
 import styles from './menu.module.css';
-
-type MenuItem = {
-  title: string;
-  subItems?: Array<MenuItem>;
-  keyCombo?: string;
-};
-
-const menuItems: Array<MenuItem> = [
-  {
-    title: 'File',
-    subItems: [
-      {
-        title: 'New',
-      },
-      {
-        title: 'Open...',
-      },
-      {
-        title: 'Save',
-      },
-      {
-        title: 'Save As...',
-      },
-      {
-        title: 'Close',
-      },
-    ],
-  },
-  {
-    title: 'Edit',
-    subItems: [
-      {
-        title: 'Cut',
-        keyCombo: 'Ctrl+X',
-      },
-      {
-        title: 'Copy',
-        keyCombo: 'Ctrl+C',
-      },
-      {
-        title: 'Paste',
-        keyCombo: 'Ctrl+V',
-      },
-      {
-        title: 'Clear',
-        keyCombo: 'Del',
-      },
-    ],
-  },
-  {
-    title: 'Search',
-  },
-  {
-    title: 'View',
-  },
-  {
-    title: 'Options',
-  },
-  {
-    title: 'Help',
-  },
-];
+import { MenuItem, menuItems } from './menu-items';
 
 export function Menu(): React.JSX.Element {
   const [activeMenuIndex, setActiveMenuIndex] = useState<number | null>(null);
+
+  function getSubItems(subItems: Array<MenuItem>): Array<React.JSX.Element> {
+    const itemMaxLength = Math.max(...subItems.map(({ title }) => title.length));
+    return subItems.map(({ title, keyCombo }) => {
+      const value = keyCombo ? `${title.padEnd(itemMaxLength + 3, ' ')} ${keyCombo}` : title;
+      return (
+        <li>
+          <button type="button">{value}</button>
+        </li>
+      );
+    });
+  }
 
   return (
     <div className={styles.menu}>
@@ -80,16 +31,7 @@ export function Menu(): React.JSX.Element {
             </button>
             {activeMenuIndex === index && subItems ? (
               <div className={styles.subMenu}>
-                <ul>
-                  {subItems.map(({ title, keyCombo }) => (
-                    <li className={keyCombo ? styles.grid : undefined}>
-                      <span>
-                        <button type="button">{title}</button>
-                      </span>
-                      {keyCombo ? <span>{keyCombo}</span> : undefined}
-                    </li>
-                  ))}
-                </ul>
+                <ul>{getSubItems(subItems)}</ul>
               </div>
             ) : undefined}
           </li>
