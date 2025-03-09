a5bbad7328af3c45cd536b82c73354752165d187
Andy <onsubmit@outlook.com>
2025-03-09 14:03:20 -0700

    Fix sub menu hit targets and dismiss menus

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,17 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import styles from './menu.module.css';
 import { MenuItem, menuItems } from './menu-items';
 
 export function Menu(): React.JSX.Element {
+  const containerRef = useRef<HTMLDivElement>(null);
   const [activeMenuIndex, setActiveMenuIndex] = useState<number | null>(null);
+
+  const handleClickOutside = (e: MouseEvent): void => {
+    if (e.target instanceof Node && !containerRef.current?.contains(e.target)) {
+      setActiveMenuIndex(null);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return (): void => document.removeEventListener('mousedown', handleClickOutside);
+  });
 
   function getSubItems(subItems: Array<MenuItem>): Array<React.JSX.Element> {
     const itemMaxLength = Math.max(...subItems.map(({ title }) => title.length));
     return subItems.map(({ title, keyCombo }) => {
       const value = keyCombo ? `${title.padEnd(itemMaxLength + 3, ' ')} ${keyCombo}` : title;
       return (
-        <li>
+        <li key={title}>
           <button type="button">{value}</button>
         </li>
       );
@@ -19,10 +31,10 @@ export function Menu(): React.JSX.Element {
   }
 
   return (
-    <div className={styles.menu}>
+    <div ref={containerRef} className={styles.menu}>
       <ul>
         {menuItems.map(({ title, subItems }, index) => (
-          <li className={activeMenuIndex === index ? styles.active : undefined}>
+          <li key={title} className={activeMenuIndex === index ? styles.active : undefined}>
             <button
               type="button"
               onClick={() => setActiveMenuIndex(activeMenuIndex !== index ? index : null)}
