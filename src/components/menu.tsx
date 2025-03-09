452346a9bb9a492677e8efe03122fbe9bfab27e0
Andy <onsubmit@outlook.com>
2025-03-09 00:28:34 -0800

    Add basis for menu

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,15 +1,77 @@
+import { useState } from 'react';
+
 import styles from './menu.module.css';
 
+type MenuItem = {
+  title: string;
+  subItems?: Array<MenuItem>;
+};
+
+const menuItems: Array<MenuItem> = [
+  {
+    title: 'File',
+    subItems: [
+      {
+        title: 'New',
+      },
+      {
+        title: 'Open...',
+      },
+      {
+        title: 'Save',
+      },
+      {
+        title: 'Save As...',
+      },
+      {
+        title: 'Close',
+      },
+    ],
+  },
+  {
+    title: 'Edit',
+  },
+  {
+    title: 'Search',
+  },
+  {
+    title: 'View',
+  },
+  {
+    title: 'Options',
+  },
+  {
+    title: 'Help',
+  },
+];
+
 export function Menu(): React.JSX.Element {
+  const [activeMenuIndex, setActiveMenuIndex] = useState<number | null>(null);
+
   return (
     <div className={styles.menu}>
       <ul>
-        <li>File</li>
-        <li>Edit</li>
-        <li>Search</li>
-        <li>View</li>
-        <li>Options</li>
-        <li>Help</li>
+        {menuItems.map(({ title, subItems }, index) => (
+          <li className={activeMenuIndex === index ? styles.active : undefined}>
+            <button
+              type="button"
+              onClick={() => setActiveMenuIndex(activeMenuIndex !== index ? index : null)}
+            >
+              {title}
+            </button>
+            {activeMenuIndex === index && subItems ? (
+              <div className={styles.subMenu}>
+                <ul>
+                  {subItems.map(({ title }) => (
+                    <li>
+                      <button type="button">{title}</button>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : undefined}
+          </li>
+        ))}
       </ul>
     </div>
   );
