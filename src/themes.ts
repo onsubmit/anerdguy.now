5a9d1c8341f378e74ab0e2c1fb32e1d6cd9d91df
Andy <onsubmit@outlook.com>
2025-03-20 20:02:56 -0700

    Add more themes

--- src/themes.ts
+++ src/themes.ts
@@ -48,7 +48,17 @@ type Theme = {
   };
 };
 
-export const themeNames = ['Default', 'Hotdog Stand'] as const;
+export const themeNames = [
+  'Default',
+  'Arizona',
+  'Black Leather Jacket',
+  'Designer',
+  'Fluorescent',
+  'Hotdog Stand',
+  'Monochrome',
+  'Ocean',
+  'Plasma',
+] as const;
 export type ThemeName = (typeof themeNames)[number];
 export type Themes = Record<ThemeName, Theme>;
 
@@ -87,14 +97,150 @@ export const themes = {
       background: 'BrWhite',
     },
   },
-  'Hotdog Stand': {
+  Arizona: {
     'Normal Text': {
       foreground: 'Black',
-      background: 'Yellow',
+      background: 'BrWhite',
+    },
+    'Selected Text': {
+      foreground: 'Black',
+      background: 'Cyan',
+    },
+    'Window Border': {
+      foreground: 'Black',
+      background: 'Brown',
+    },
+    Menubar: {
+      foreground: 'BrWhite',
+      background: 'Cyan',
+    },
+    'Status Line': {
+      foreground: 'Black',
+      background: 'White',
+    },
+    Dialogs: {
+      foreground: 'Black',
+      background: 'BrWhite',
+    },
+    'Dialog Titlebar': {
+      foreground: 'BrWhite',
+      background: 'Cyan',
+    },
+    'Dialog Buttons': {
+      foreground: 'BrWhite',
+      background: 'Brown',
+    },
+  },
+  'Black Leather Jacket': {
+    'Normal Text': {
+      foreground: 'Black',
+      background: 'White',
     },
     'Selected Text': {
       foreground: 'BrWhite',
+      background: 'Magenta',
+    },
+    'Window Border': {
+      foreground: 'BrWhite',
+      background: 'Gray',
+    },
+    Menubar: {
+      foreground: 'BrWhite',
+      background: 'Magenta',
+    },
+    'Status Line': {
+      foreground: 'BrWhite',
       background: 'Black',
+    },
+    Dialogs: {
+      foreground: 'Black',
+      background: 'BrWhite',
+    },
+    'Dialog Titlebar': {
+      foreground: 'BrWhite',
+      background: 'Magenta',
+    },
+    'Dialog Buttons': {
+      foreground: 'BrWhite',
+      background: 'Magenta',
+    },
+  },
+  Designer: {
+    'Normal Text': {
+      foreground: 'Black',
+      background: 'White',
+    },
+    'Selected Text': {
+      foreground: 'Black',
+      background: 'Cyan',
+    },
+    'Window Border': {
+      foreground: 'Black',
+      background: 'BrWhite',
+    },
+    Menubar: {
+      foreground: 'BrWhite',
+      background: 'Cyan',
+    },
+    'Status Line': {
+      foreground: 'BrWhite',
+      background: 'Black',
+    },
+    Dialogs: {
+      foreground: 'Black',
+      background: 'BrWhite',
+    },
+    'Dialog Titlebar': {
+      foreground: 'BrWhite',
+      background: 'Cyan',
+    },
+    'Dialog Buttons': {
+      foreground: 'BrWhite',
+      background: 'Cyan',
+    },
+  },
+  Fluorescent: {
+    'Normal Text': {
+      foreground: 'Black',
+      background: 'BrWhite',
+    },
+    'Selected Text': {
+      foreground: 'Black',
+      background: 'Cyan',
+    },
+    'Window Border': {
+      foreground: 'Black',
+      background: 'BrGreen',
+    },
+    Menubar: {
+      foreground: 'Black',
+      background: 'Pink',
+    },
+    'Status Line': {
+      foreground: 'BrWhite',
+      background: 'Black',
+    },
+    Dialogs: {
+      foreground: 'Black',
+      background: 'BrWhite',
+    },
+    'Dialog Titlebar': {
+      foreground: 'Black',
+      background: 'Pink',
+    },
+    'Dialog Buttons': {
+      foreground: 'Black',
+      background: 'BrGreen',
+    },
+  },
+  'Hotdog Stand': {
+    'Normal Text': {
+      foreground: 'Red',
+      background: 'Yellow',
+    },
+    'Selected Text': {
+      foreground: 'Yellow',
+      background: 'Red',
     },
     'Window Border': {
       foreground: 'Yellow',
@@ -119,6 +265,108 @@ export const themes = {
     'Dialog Buttons': {
       foreground: 'Yellow',
       background: 'Red',
+    },
+  },
+  Monochrome: {
+    'Normal Text': {
+      foreground: 'Black',
+      background: 'BrWhite',
+    },
+    'Selected Text': {
+      foreground: 'Black',
+      background: 'White',
+    },
+    'Window Border': {
+      foreground: 'Black',
+      background: 'BrWhite',
+    },
+    Menubar: {
+      foreground: 'BrWhite',
+      background: 'Black',
+    },
+    'Status Line': {
+      foreground: 'Black',
+      background: 'White',
+    },
+    Dialogs: {
+      foreground: 'Black',
+      background: 'BrWhite',
+    },
+    'Dialog Titlebar': {
+      foreground: 'BrWhite',
+      background: 'Black',
+    },
+    'Dialog Buttons': {
+      foreground: 'Black',
+      background: 'White',
+    },
+  },
+  Ocean: {
+    'Normal Text': {
+      foreground: 'Black',
+      background: 'Cyan',
+    },
+    'Selected Text': {
+      foreground: 'Black',
+      background: 'BrCyan',
+    },
+    'Window Border': {
+      foreground: 'Black',
+      background: 'White',
+    },
+    Menubar: {
+      foreground: 'BrWhite',
+      background: 'Blue',
+    },
+    'Status Line': {
+      foreground: 'BrWhite',
+      background: 'Black',
+    },
+    Dialogs: {
+      foreground: 'Black',
+      background: 'BrWhite',
+    },
+    'Dialog Titlebar': {
+      foreground: 'BrWhite',
+      background: 'Blue',
+    },
+    'Dialog Buttons': {
+      foreground: 'BrWhite',
+      background: 'Blue',
+    },
+  },
+  Plasma: {
+    'Normal Text': {
+      foreground: 'BrWhite',
+      background: 'Black',
+    },
+    'Selected Text': {
+      foreground: 'Black',
+      background: 'Pink',
+    },
+    'Window Border': {
+      foreground: 'Black',
+      background: 'Pink',
+    },
+    Menubar: {
+      foreground: 'BrWhite',
+      background: 'Blue',
+    },
+    'Status Line': {
+      foreground: 'BrWhite',
+      background: 'Black',
+    },
+    Dialogs: {
+      foreground: 'Black',
+      background: 'BrWhite',
+    },
+    'Dialog Titlebar': {
+      foreground: 'Black',
+      background: 'Pink',
+    },
+    'Dialog Buttons': {
+      foreground: 'Black',
+      background: 'Pink',
     },
   },
 } as const satisfies Themes;
